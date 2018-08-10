04c8ff4a8c14d3eee38a79ef37451cf16bfb2eee
Saujas Adarkar <saujas.adarkar@gmail.com>
2018-08-10 10:44:21 +0530

    Minor changes

--- website/basic/views.py
+++ website/basic/views.py
@@ -1,16 +1,17 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from .models import BasicUser
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from datetime import datetime
+from ipware.ip import get_ip
 
 def index(request):
 	var = False
 	if request.POST:
 		name=request.POST['name']
 		email=request.POST['email']
-		ip_address = request.META['REMOTE_ADDR']
+		#ip_address = request.META['HTTP_X_FORWARDED_FOR'].split(",")[0].strip()
+		ip_address = get_ip(request)
 		obj = BasicUser(name=name, email=email, ip=ip_address)
 		obj.save()
 		var = True
